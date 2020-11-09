4ee7446a9dbf263d8d1d077e0fefee6247153e8f
Bhattacharya <shreya.bhattacharya@dxc.com>
2020-11-09 18:07:28 +0530

    first commit

---
+++ jenkindemo/src/main/java/com/sj/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.sj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	
+	@GetMapping("/hi")
+	public String sayHi() {
+			
+		return "hii.. everyone";
+
+	}
+
+}
